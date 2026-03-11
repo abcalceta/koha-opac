ce1087e094106b3a0bc6027da2da3578af778ec5
PSSC <pssc@PSSCs-MacBook-Air.local>
2026-03-11 16:58:18 +0800

    custom homepage

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,46 @@
+function loadRandomBooks(){
+
+fetch("/cgi-bin/koha/svc/report?id=6")
+.then(r=>r.json())
+.then(data=>{
+
+let html="";
+
+data.forEach(row=>{
+
+let biblio=row[0];
+let title=row[1]||"Book";
+
+html+=`
+<a class="random-book"
+href="/cgi-bin/koha/opac-detail.pl?biblionumber=${biblio}">
+<div class="bookcover" data-title="${title}"></div>
+<span class="booktitle">${title}</span>
+</a>
+`;
+
+});
+
+document.querySelector("#random-books").innerHTML=html;
+
+generateMissingCovers();
+
+});
+
+}
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+if(document.body.id==="opac-main"){
+
+fetch("https://abcalceta.github.io/koha-opac/html/homepage.html")
+.then(r=>r.text())
+.then(html=>{
+
+document.querySelector("#OpacMainUserBlock").innerHTML=html;
+
+/* enable shelf buttons AFTER html exists */
 
 document.querySelector(".scroll-btn.left")?.addEventListener("click",()=>{
 document.querySelector(".discover-shelf").scrollBy({left:-400,behavior:"smooth"});
@@ -7,55 +50,12 @@ document.querySelector(".scroll-btn.right")?.addEventListener("click",()=>{
 document.querySelector(".discover-shelf").scrollBy({left:400,behavior:"smooth"});
 });
 
+/* load books */
 
+loadRandomBooks();
 
-function loadRandomBooks(){
-
-    fetch("/cgi-bin/koha/svc/report?id=6")
-    .then(r => r.json())
-    .then(data => {
-
-        let html = "";
-
-        data.forEach(row => {
-
-            let biblio = row[0];
-            let title = row[1] || "Book";
-
-            html += `
-            <a class="random-book"
-               href="/cgi-bin/koha/opac-detail.pl?biblionumber=${biblio}">
-                <div class="bookcover" data-title="${title}"></div>
-                <span class="booktitle">${title}</span>
-            </a>
-            `;
-
-        });
-
-        document.querySelector("#random-books").innerHTML = html;
-
-        generateMissingCovers();
-
-    });
+});
 
 }
-
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    if(document.body.id === "opac-main"){
-
-        fetch("https://abcalceta.github.io/koha-opac/html/homepage.html")
-        .then(r=>r.text())
-        .then(html=>{
-
-            document.querySelector("#OpacMainUserBlock").innerHTML = html;
-
-            /* start shelf */
-            loadRandomBooks();
-
-        });
-
-    }
 
 });
