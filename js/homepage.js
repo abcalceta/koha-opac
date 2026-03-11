e4572c6e16a1628981257c81c5f02297b78f1a82
PSSC <pssc@PSSCs-MacBook-Air.local>
2026-03-11 16:15:57 +0800

    custom homepage

--- js/homepage.js
+++ js/homepage.js
@@ -9,15 +9,13 @@ document.querySelector(".discover-shelf").scrollBy({left:400,behavior:"smooth"})
 document.addEventListener("DOMContentLoaded", () => {
 
     /* only run on homepage */
+	fetch("https://abcalceta.github.io/koha-opac/html/homepage.html")
+	.then(r=>r.text())
+	.then(html=>{
+	    document.querySelector("#OpacMainUserBlock").innerHTML = html;
 
-    if(document.body.id === "opac-main"){
-
-        fetch("https://USERNAME.github.io/koha-opac/html/homepage.html")
-        .then(r=>r.text())
-        .then(html=>{
-            document.querySelector("#OpacMainUserBlock").innerHTML = html;
-        });
-
-    }
+	    /* NOW run the random books loader */
+	    loadRandomBooks();
+	});
 
 });
