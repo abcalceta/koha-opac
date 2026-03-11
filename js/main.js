f822a1cc84e0f868dba09ceee40ab8a85790af13
PSSC <pssc@PSSCs-MacBook-Air.local>
2026-03-11 16:09:18 +0800

    various fixes

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ import "./homepage.js";
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Koha OPAC enhancements loaded");
 });
+
+let url = "https://USERNAME.github.io/koha-opac/html/homepage.html?v=" + Date.now();
+
+fetch(url)
+.then(r=>r.text())
+.then(html=>{
+document.querySelector("#OpacMainUserBlock").innerHTML = html;
+});
