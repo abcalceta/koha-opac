664d418b58c7ac92bbb79d069145ca5b45aaeaa0
PSSC <pssc@PSSCs-MacBook-Air.local>
2026-03-12 06:02:38 +0800

    book thumbnails

--- js/homepage.js
+++ js/homepage.js
@@ -2,10 +2,29 @@ export const homepageHTML = `
 <h2>Discover Books</h2>
 
 <div class="discover-wrapper">
-<button class="scroll-btn left">‹</button>
 
-<div id="random-books" class="discover-shelf"></div>
+    <button class="scroll-btn left">‹</button>
 
-<button class="scroll-btn right">›</button>
+    <div id="random-books" class="discover-shelf"></div>
+
+    <button class="scroll-btn right">›</button>
+
+</div>
+
+<h2>Anthropology Highlights</h2>
+
+<div class="discover-wrapper">
+
+    <button class="scroll-btn left">‹</button>
+
+    <div id="anthro-books" class="discover-shelf"></div>
+
+    <button class="scroll-btn right">›</button>
+
 </div>
 `;
+
+
+
+loadShelf("random-books", 6);     // report id for random books
+loadShelf("anthro-books", 7);     // report id for anthropology
