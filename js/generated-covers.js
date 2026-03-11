45295f99508161d6935a2e63f600fbad04c4a6cc
PSSC <pssc@PSSCs-MacBook-Air.local>
2026-03-11 16:20:03 +0800

    custom homepage

--- js/generated-covers.js
+++ js/generated-covers.js
@@ -1,3 +1,26 @@
+function createGeneratedCover(title){
+
+    let div = document.createElement("div");
+    div.className = "generated-cover";
+
+    let hash = 0;
+    for(let i=0;i<title.length;i++){
+        hash = title.charCodeAt(i) + ((hash<<5)-hash);
+    }
+
+    let hue = 220 + (Math.abs(hash) % 60);
+
+    div.style.background = `linear-gradient(
+        135deg,
+        hsl(${hue},60%,60%),
+        hsl(${hue+15},65%,45%)
+    )`;
+
+    div.innerText = title;
+
+    return div;
+}
+
 function ensureCovers(){
 
 document.querySelectorAll(".bookcover").forEach(el=>{
