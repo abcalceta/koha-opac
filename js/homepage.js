c468b68e40027b16f503bbdf42d16bd847889973
PSSC <pssc@PSSCs-MacBook-Air.local>
2026-03-11 16:44:30 +0800

    custom homepage

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,36 @@
 
 
+function loadRandomBooks(){
+
+    fetch("/cgi-bin/koha/svc/report?id=6")
+    .then(r => r.json())
+    .then(data => {
+
+        let html = "";
+
+        data.forEach(row => {
+
+            let biblio = row[0];
+            let title = row[1] || "Book";
+
+            html += `
+            <a class="random-book"
+               href="/cgi-bin/koha/opac-detail.pl?biblionumber=${biblio}">
+                <div class="bookcover" data-title="${title}"></div>
+                <span class="booktitle">${title}</span>
+            </a>
+            `;
+
+        });
+
+        document.querySelector("#random-books").innerHTML = html;
+
+        generateMissingCovers();
+
+    });
+
+}
+
 document.querySelector(".scroll-btn.left")?.addEventListener("click",()=>{
 document.querySelector(".discover-shelf").scrollBy({left:-400,behavior:"smooth"});
 });
