ef98cc1171aa30f4581269cde9b11e26e018d32c
PSSC <pssc@PSSCs-MacBook-Air.local>
2026-03-12 09:05:02 +0800

    version 1.1.0

--- js/main.js
+++ js/main.js
@@ -49,14 +49,6 @@ if(document.readyState !== "loading"){
 }
 
 
-
-if(document.readyState !== "loading"){
-    init();
-}else{
-    document.addEventListener("DOMContentLoaded", init);
-}
-
-
 function loadCSS(){
 
     const css=document.createElement("link");
